5fefa4eefe4d5b374d1be08d2328e4847a2c7170
Zhaoyu SUN <zhaoyu.sun@creditcloud.com>
2015-06-11 17:38:44 +0800

    thymeleaf

--- src/main/java/com/brick/service/CurrentUserServiceImpl.java
+++ src/main/java/com/brick/service/CurrentUserServiceImpl.java
@@ -1,5 +1,21 @@
 package com.brick.service;
 
-public class CurrentUserServiceImpl {
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.stereotype.Service;
+
+import com.brick.entities.CurrentUser;
+import com.brick.entities.Role;
+
+@Service
+@Slf4j
+public class CurrentUserServiceImpl implements CurrentUserService {
+
+    @Override
+    public boolean canAccessUser(CurrentUser currentUser, Long userId) {
+        log.debug("Checking if user={} has access to user={}", currentUser, userId);
+        return currentUser != null
+                && (currentUser.getRole() == Role.ADMIN || currentUser.getId().equals(userId));
+    }
 
 }
